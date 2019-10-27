d48babc1c961c97e6c2508be463eb225cedee276
Andrey Kostyukov <kostleand@gmail.com>
2019-10-27 16:13:16 +0200

    firebase connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rx-jira';
+  items$: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items$ = db.collection('users').valueChanges();
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCNnPFGnqNKsiZwWyVaGjsfjLaXG2H9aa0',
+    authDomain: 'angular-jira-c95c4.firebaseapp.com',
+    databaseURL: 'https://angular-jira-c95c4.firebaseio.com',
+    projectId: 'angular-jira-c95c4',
+    storageBucket: 'angular-jira-c95c4.appspot.com',
+    messagingSenderId: '251143550927',
+    appId: '1:251143550927:web:e4bbfe7b461b2a6ce204ba'
+  }
 };
 
 /*
