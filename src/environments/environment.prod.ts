3081997eda8f9c9259560f711e850c614cc83c2d
Andrey Kostyukov <kostleand@gmail.com>
2019-10-27 16:37:15 +0200

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCNnPFGnqNKsiZwWyVaGjsfjLaXG2H9aa0',
+    authDomain: 'angular-jira-c95c4.firebaseapp.com',
+    databaseURL: 'https://angular-jira-c95c4.firebaseio.com',
+    projectId: 'angular-jira-c95c4',
+    storageBucket: 'angular-jira-c95c4.appspot.com',
+    messagingSenderId: '251143550927',
+    appId: '1:251143550927:web:e4bbfe7b461b2a6ce204ba'
+  }
 };
